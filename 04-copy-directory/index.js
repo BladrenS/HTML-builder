4387abe69818789ae78f94be43f11baac5141445
[BladrenS] <denis_ka_98@mail.ru>
2025-01-17 00:53:18 +0600

    feat: add 4th task solution

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+const path = require('path');
+async function copy() {
+  try {
+    const sourceDir = path.join(__dirname, 'files');
+    const targetDir = path.join(__dirname, 'files-copy');
+    await fs.mkdir(targetDir, { recursive: true });
+    const files = await fs.readdir(sourceDir);
+    for (const file of files) {
+      const sourceFilePath = path.join(sourceDir, file);
+      const targetFilePath = path.join(targetDir, file);
+      const stats = await fs.stat(sourceFilePath);
+      if (stats.isFile()) {
+        await fs.copyFile(sourceFilePath, targetFilePath);
+      }
+    }
+  } catch (error) {
+    console.error('Something`s wrong! Error:', error);
+  }
+}
+copy();
