b92ced2775fed58f180dfeeaba4d38ae362f4071
[BladrenS] <denis_ka_98@mail.ru>
2025-01-16 18:27:55 +0600

    feat: add solutions for 1 and 2 tasks

---
+++ 02-write-file/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const filePath = path.join(__dirname, 'output.txt');
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+const readLine = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Hello! Ola! Write you message here! :)');
+
+readLine.on('line', (input) => {
+  if (input.trim().toLowerCase() === 'exit') {
+    exitTask();
+  } else {
+    writeStream.write(`${input}\n`);
+    console.log('Got it! Write your next message or type "exit" to leave.');
+  }
+});
+
+process.on('SIGINT', handleCtrlC);
+function handleCtrlC() {
+  console.log('That`s all? I`ll be glad to see you again! See ya! :)');
+  writeStream.end(() => {
+    readLine.close();
+    process.exit(0);
+  });
+}
+
+function exitTask() {
+  console.log('That`s all? I`ll be glad to see you again! See ya! :)');
+  readLine.close();
+  writeStream.end(() => process.exit());
+}
