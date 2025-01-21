a881e92eeccc9f59597e4c1275ac6ed6ba2ee0cf
[BladrenS] <denis_ka_98@mail.ru>
2025-01-21 20:31:24 +0600

    fix: 5 task fix

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const stylesFolder = path.join(__dirname, 'styles');
 const outputFolder = path.join(__dirname, 'project-dist');
@@ -6,19 +6,18 @@ const outputFile = path.join(outputFolder, 'bundle.css');
 
 async function summStyles() {
   try {
-    fs.writeFileSync(outputFile, '', 'utf-8');
-    const files = await fs.promises.readdir(stylesFolder, {
-      withFileTypes: true,
-    });
+    await fs.writeFile(outputFile, '', 'utf-8');
+    const files = await fs.readdir(stylesFolder, { withFileTypes: true });
     for (const file of files) {
       const filePath = path.join(stylesFolder, file.name);
       if (path.extname(file.name) === '.css' && file.isFile()) {
-        const content = await fs.promises.readFile(filePath, 'utf-8');
-        fs.appendFileSync(outputFile, content + '\n', 'utf-8');
+        const content = await fs.readFile(filePath, 'utf-8');
+        await fs.appendFile(outputFile, content + '\n', 'utf-8');
       }
     }
   } catch (error) {
     console.error('Woops, something went wrong! Error:', error);
   }
 }
+
 summStyles();
